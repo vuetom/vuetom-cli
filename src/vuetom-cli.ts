d378e8fdb4ac4c41b3c3983fcc49f7c1a7c26cf0
lauset <liujie_783@163.com>
2022-05-20 23:28:40 +0800

    feat: new command 'git'

--- src/vuetom-cli.ts
+++ src/vuetom-cli.ts
@@ -7,40 +7,29 @@ program
   .version(require('../package').version)
   .usage('<command> [options]')
   // .option('-i, init [name]', 'init vitepress-theme-vuetom theme')
-  .command('init', 'generate a new project from a template')
-  .command('list', 'list available official templates')
+  .command('init <project-name>', 'generate a new project from a template')
+  .command('list [branch]', 'list available official templates by branch')
 
-// program
-//   .command('list', 'list desc')
-//   .action(function () {
-//     list()
-//   })
+program
+  .command('git <user> [type]')
+  .description('show the list of github repository of a user')
+  .action((user: any, type: any) => {
+    require('./cmd-git').default(user, type)
+  })
 
-// program
-//   .command('init', 'init desc')
-//   .action(function () {
-//     init()
-//   })
-
-/**
- * Help
- */
 program.on('--help', () => {
   console.log('  Examples:')
   console.log()
   console.log(chalk.gray('    # create a new project with an official template'))
   console.log('    $ vuetom-cli init project-name')
   console.log()
-  console.log(chalk.gray('    # show github template list'))
-  console.log('    $ vuetom-cli list')
+  console.log(chalk.gray('    # show github branch information'))
+  console.log('    $ vuetom-cli list temp-docs')
+  console.log()
+  console.log(chalk.gray('    # show github repository'))
+  console.log('    $ vuetom-cli git vuejs')
   console.log()
 })
-
-// function help () {
-//   program.parse(process.argv)
-//   if (program.args.length < 1) return program.help()
-// }
-// help()
 
 program.parse(process.argv)
 
