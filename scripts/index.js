f12e3f756f90703d5ebb883974c831879ce17e69
lauset <liujie_783@163.com>
2022-09-03 22:39:37 +0800

    chore: upgrade deps

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ async function copyDocs () {
     fs.copySync('../temp-docs', '../cli/temp-docs')
     fs.removeSync('../cli/temp-docs/node_modules')
     fs.removeSync('../cli/temp-docs/package-lock.json')
-  }, 1000)
+  }, 500)
 }
 
 async function copyBlog () {
@@ -17,12 +17,28 @@ async function copyBlog () {
     fs.copySync('../temp-blog', '../cli/temp-blog')
     fs.removeSync('../cli/temp-blog/node_modules')
     fs.removeSync('../cli/temp-blog/package-lock.json')
-  }, 1000)
+  }, 500)
+}
+
+async function copyOther () {
+  const files = [
+    'README.md',
+    'CHANGELOG.md',
+    'media'
+  ]
+  files.forEach(f => {
+    fs.removeSync(`../cli/${f}`)
+    console.log(`start copy ${f}`)
+    setTimeout(() => {
+      fs.copySync(`../../${f}`, `../cli/${f}`)
+    }, 1000)
+  })
 }
 
 async function copy () {
   await copyDocs()
   await copyBlog()
+  await copyOther()
 }
 
 copy()
